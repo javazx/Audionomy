2ec16175e20193ac6403a68fffe3476d4088ec67
javazx <31910259+javazx@users.noreply.github.com>
2018-01-11 17:09:36 -0600

    Primera Version

---
+++ app/src/main/java/audionomy/javax/valeriano/audionomy/MainActivity.java
@@ -0,0 +1,71 @@
+package audionomy.javax.valeriano.audionomy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Load an ad into the AdMob banner view.
+        MobileAds.initialize(this, "ca-app-pub-7927185138091691~6790721982");
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                Log.i("Ads", "onAdLoaded");
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                Log.i("Ads", "onAdFailedToLoad");
+                Log.i("Ads", String.valueOf(errorCode));
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+                Log.i("Ads", "onAdOpened");
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+                Log.i("Ads", "onAdLeftApplication");
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the user is about to return
+                // to the app after tapping on an ad.
+                Log.i("Ads", "onAdClosed");
+            }
+        });
+        webView = (WebView) findViewById(R.id.webView1);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(false);
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+
+        //webView.loadData("<iframe  width=\"100%\" height=\"75%\" border=\"0\" style=\"border: 0; box-shadow: 0px 0px 0  #fff; src=\"http://radiohosting.online/9478/index.php\"></iframe>", "text/html", null);
+        webView.loadUrl("http://radiohosting.online/9478/index.php");
+    }
+}
